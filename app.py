12f933cf3b4c6eca45b22360cce6ce3198dc9a8c
Patryk <patryk.p.janas@gmail.com>
2020-07-11 18:35:34 +0200

    Cleaning code

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify, request
-from flask_restplus import Api, Resource
+from flask_restplus import Api
 from datetime import datetime, timezone, timedelta
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
-from flask_jwt_extended import JWTManager, decode_token
+from flask_jwt_extended import JWTManager
 
 
-app = Flask(__name__)  # Flask Application
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site_new.db'
-# Create RestPlus API
 api = Api(app)
-# SQLAlchemy Database instance
 db = SQLAlchemy(app)
 flask_bcrypt = Bcrypt(app)
-# login_manager = LoginManager(app)
 CORS(app)
 JWTManager(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['JWT_SECRET_KEY'] = 'FC01D1F2EA853CE3E6995DF1DF50C0DA0E618369B1EE8078202C630376577109'
-# normally you would import models here
-# ex: from app.data import models
 
 
 class User(db.Model):
@@ -44,39 +39,7 @@ class LoginDateTime(db.Model):
         return f"Login date('{self.login_date}')"
 
 
-# Bind the SQLAlchemy to the Flask Application
 db.init_app(app)
-
-# Link Flask Rest Plus API with SQLAlchemy
-# api_model_factory = ApiModelFactory(api=api, db=db)
-
-# Get entity Person
-# model = api_model_factory.get_entity(User.__tablename__)
-
-
-@api.route('/userinfo')
-class Login(Resource):
-    def get(self):
-        user_data = {
-            'name': User.query.filter_by(username='pjanas').first().name,
-            'surname': User.query.filter_by(username='pjanas').first().surname,
-            'username': User.query.filter_by(username='pjanas').first().username,
-            'password': User.query.filter_by(username='pjanas').first().password,
-            'token': User.query.filter_by(username='pjanas').first().token
-        }
-
-        user = jsonify(user_data)
-        return user
-
-    def post(self):
-        user_data = {
-            'name': User.query.filter_by(username='pjanas').first().name,
-            'surname': User.query.filter_by(username='pjanas').first().surname,
-            'username': User.query.filter_by(username='pjanas').first().username,
-            'password': User.query.filter_by(username='pjanas').first().password
-        }
-        user = jsonify(user_data)
-        return user
 
 
 @app.route('/home', methods=["POST"])
@@ -84,26 +47,19 @@ def create_entry():
     req = request.get_json()
     username = req.get('username')
     password = req.get('password')
-    # print(req)
+    resp = ''
     try:
 
         username_check = username == User.query.filter_by(username=username).first().username
         password_check = flask_bcrypt.check_password_hash(User.query.filter_by(username=username).first().password, password.encode('utf-8'))
 
         if username_check and password_check:
-            name = User.query.filter_by(username=username).first().name
-            surname = User.query.filter_by(username=username).first().surname
-
             access_token = User.query.filter_by(username=username).first().token
-
-            # print(name)
-            # print(surname)
-            # print(access_token)
             resp = {'token': access_token}
 
     except:
 
-        access_token = {'status': 'nok'}
+        resp = {'status': 'nok'}
 
     return resp
 
@@ -112,23 +68,26 @@ def create_entry():
 @cross_origin()
 def show():
     req = request.get_json()
-    token = req.get('token')
-    user_id = User.query.filter_by(token=token).first().id
-    log = LoginDateTime(login_date=datetime.now(timezone(timedelta(hours=2))), user_id=user_id)
-    db.session.add(log)
-    db.session.commit()
-    print(req)
-    print(LoginDateTime.query.all())
-    resp = []
+    try:
+        token = req.get('token')
+        user_id = User.query.filter_by(token=token).first().id
+        log = LoginDateTime(login_date=datetime.now(timezone(timedelta(hours=2))), user_id=user_id)
+        db.session.add(log)
+        db.session.commit()
+        print(req)
+        print(LoginDateTime.query.all())
+        resp = []
 
-    for row in LoginDateTime.query.all():
-        if row.user_id == user_id:
-            info = {
-                'name': User.query.filter_by(id=user_id).first().name,
-                'surname': User.query.filter_by(id=user_id).first().surname,
-                'date': row.login_date
-            }
-            print(info)
-            resp.append(info)
-    print(resp)
+        for row in LoginDateTime.query.all():
+            if row.user_id == user_id:
+                info = {
+                    'name': User.query.filter_by(id=user_id).first().name,
+                    'surname': User.query.filter_by(id=user_id).first().surname,
+                    'date': row.login_date
+                }
+                print(info)
+                resp.append(info)
+        print(resp)
+    except:
+        resp = {'error': 'wrong token'}
     return jsonify(resp)
--- test.py
+++ test.py
@@ -1,46 +1,56 @@
 from app import app
 from flask import json
+from app import User
 import unittest
 
 
 class TestCase(unittest.TestCase):
 
-    # Ensure that Flask was set up correctly
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
-
-    # Ensure that main page requires user login
 
     def test_correct_login(self):
         tester = app.test_client(self)
 
         response = tester.post('/home',
                                data=json.dumps({"username": "pjanas", "password": "asdqwe123"}), content_type='application/json', follow_redirects=True)
-        print(response.data.decode("utf-8"))
         decoded_response = json.loads(response.data.decode("utf-8"))
-        res = 'ok' == decoded_response.get('status')
-        print(res)
-        self.assertTrue(res)
+        token = decoded_response.get('token')
+        if User.query.filter_by(username='pjanas').first().token == token:
+            check = True
+        else:
+            check = False
+        self.assertTrue(check)
 
     def test_wrong_login(self):
         tester = app.test_client(self)
 
         response = tester.post('/home',
-                               data=json.dumps({"username": "wronguser", "password": "wrongpassword"}), content_type='application/json', follow_redirects=True)
-        print(response.data.decode("utf-8"))
+                               data=json.dumps({"username": "wronguser", "password": "wrongpassword"}),
+                               content_type='application/json', follow_redirects=True)
         decoded_response = json.loads(response.data.decode("utf-8"))
-        res = 'nok' == decoded_response.get('status')
-        print(res)
-        self.assertTrue(res)
+        check = 'nok' == decoded_response.get('status')
+        self.assertTrue(check)
 
-    # # Ensure that welcome page loads
-    # def test_welcome_route_works_as_expected(self):
-    #     response = self.client.get('/welcome', follow_redirects=True)
-    #     self.assertIn(b'Welcome to Flask!', response.data)
-    #
+    def test_session_wrong_token(self):
+        tester = app.test_client(self)
+        response = tester.post('/history',
+                               data=json.dumps({"token": "wrongtoken"}),
+                               content_type='application/json', follow_redirects=True)
+        decoded_response = json.loads(response.data.decode("utf-8"))
+        check = 'wrong token' == decoded_response.get('error')
+        self.assertTrue(check)
 
+    def test_session_no_token(self):
+        tester = app.test_client(self)
+        response = tester.post('/history',
+                               data=json.dumps({}),
+                               content_type='application/json', follow_redirects=True)
+        decoded_response = json.loads(response.data.decode("utf-8"))
+        check = 'wrong token' == decoded_response.get('error')
+        self.assertTrue(check)
 
 if __name__ == '__main__':
     unittest.main()
